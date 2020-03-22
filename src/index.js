1b1be74beeb3c26a74ac870b226fbc848533414c
Dzmitry Stolbou <persik1223@gmail.com>
2020-03-22 18:06:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length) {
+        let min = array[0];
+        array.forEach((item) => {
+            if (item < min) min = item;
+        })
+        return min;
+    } else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length) {
+        let max = array[0];
+        array.forEach((item) => {
+            if (item > max) max = item;
+        })
+        return max;
+    } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length) {
+        return array.reduce((acc, curr) => acc + curr) / array.length;
+    } else return 0;
 }
